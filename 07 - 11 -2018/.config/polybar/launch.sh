5a1c41ac75ab3f79f0cd9ae30fa232a72d12f755
banbeucmas <banbeucams@gmail.com>
2018-11-07 17:32:38 +0700

    Added November Rice (07 - 11 - 2018)

---
+++ 07 - 11 -2018/.config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar -r bottom_left & polybar -r bottom_right & polybar -r bottom_mid &
+polybar -r top_left & polybar -r top_right & polybar -r top_mid &
+
+echo "Bars launched..."
